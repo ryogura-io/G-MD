796b72aa13e2709f34a59dc9d922e3829d33acc0
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-08-23 16:55:04 +0100

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -2,8 +2,6 @@ const axios = require('axios');
 
 module.exports = async function (sock, chatId, city, message) {
     try {
-// React first
-        await sock.sendMessage(chatId, {react: { text: "☁️", key: message.key }});
         const apiKey = '4902c0f2550f58298ad4146a92b65e10';  // Replace with your OpenWeather API Key
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
         const weather = response.data;
