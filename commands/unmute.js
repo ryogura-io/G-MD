943b4a92ccbd2b610a748ef377d91ff84661d781
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-08-23 11:20:57 +0100

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,4 +1,6 @@
-async function unmuteCommand(sock, chatId) {
+async function unmuteCommand(sock, chatId, message) {
+// React first
+        await sock.sendMessage(chatId, {react: { text: "🔓", key: message.key }});
     await sock.groupSettingUpdate(chatId, 'not_announcement'); // Unmute the group
     await sock.sendMessage(chatId, { text: 'The group has been unmuted.' });
 }
