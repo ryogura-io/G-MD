8902929e7498bf3ee92f3b4bbbd96378c685b86d
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-08-23 23:33:08 +0100

    Update dare.js

--- commands/dare.js
+++ commands/dare.js
@@ -2,18 +2,17 @@ const fetch = require('node-fetch');
 
 async function dareCommand(sock, chatId, message) {
     try {
-        const shizokeys = 'knightbot';
-        const res = await fetch(`https://api.shizo.top/api/quote/dare?apikey=${shizokeys}`);
+        const res = await fetch("https://api.truthordarebot.xyz/v1/dare");
         
         if (!res.ok) {
             throw await res.text();
         }
-        
+
         const json = await res.json();
-        const dareMessage = json.result;
+        const dareMessage = json.question;
 
         // Send the dare message
-        await sock.sendMessage(chatId, { text: dareMessage }, { quoted: message });
+        await sock.sendMessage(chatId, { text: `⚡ *Dare:* ${dareMessage}` }, { quoted: message });
     } catch (error) {
         console.error('Error in dare command:', error);
         await sock.sendMessage(chatId, { text: '❌ Failed to get dare. Please try again later!' }, { quoted: message });
