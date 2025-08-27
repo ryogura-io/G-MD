0eaf8cc634a18d98b7af074caccfab7d5da167e2
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-08-27 17:10:29 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { channelInfo } = require('../lib/messageConfig');
 
 async function banCommand(sock, chatId, message) {
     let userToBan;
@@ -16,7 +15,6 @@ async function banCommand(sock, chatId, message) {
     if (!userToBan) {
         await sock.sendMessage(chatId, { 
             text: 'Please mention the user or reply to their message to ban!', 
-            ...channelInfo 
         });
         return;
     }
@@ -31,18 +29,16 @@ async function banCommand(sock, chatId, message) {
             await sock.sendMessage(chatId, { 
                 text: `Successfully banned @${userToBan.split('@')[0]}!`,
                 mentions: [userToBan],
-                ...channelInfo 
             });
         } else {
             await sock.sendMessage(chatId, { 
                 text: `${userToBan.split('@')[0]} is already banned!`,
                 mentions: [userToBan],
-                ...channelInfo 
             });
         }
     } catch (error) {
         console.error('Error in ban command:', error);
-        await sock.sendMessage(chatId, { text: 'Failed to ban user!', ...channelInfo });
+        await sock.sendMessage(chatId, { text: 'Failed to ban user!',});
     }
 }
 
