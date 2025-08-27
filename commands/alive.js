695ec78445504d9dd99bacea6cfc5133e83bb9dc
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-08-27 17:13:01 +0100

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,4 +1,8 @@
 const settings = require("../settings");
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync('./data/messageCount.json'));
+const currentMode = data.isPublic ? 'PUBLIC' : 'PRIVATE';
 
 async function aliveCommand(sock, chatId, message) {
     try {
@@ -6,7 +10,7 @@ async function aliveCommand(sock, chatId, message) {
         const message1 = `*Gura Bot is Active!*\n\n` +
                        `*Version:* ${settings.version}\n` +
                        `*Status:* Online\n` +
-                       `*Mode:* Public`;
+                       `*Mode:* ${currentMode}`;
 
         await sock.sendMessage(chatId, {
             text: message1,
