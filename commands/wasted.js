6dabe3ff43967ba060a500f42485e252cdbee703
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-08-27 17:16:21 +0100

    Update wasted.js

--- commands/wasted.js
+++ commands/wasted.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const { channelInfo } = require('../lib/messageConfig');
 
 async function wastedCommand(sock, chatId, message) {
     let userToWaste;
@@ -16,7 +15,7 @@ async function wastedCommand(sock, chatId, message) {
     if (!userToWaste) {
         await sock.sendMessage(chatId, { 
             text: 'Please mention someone or reply to their message to waste them!', 
-            ...channelInfo 
+            
         });
         return;
     }
@@ -41,14 +40,13 @@ async function wastedCommand(sock, chatId, message) {
             image: Buffer.from(wastedResponse.data),
             caption: `⚰️ *Wasted* : ${userToWaste.split('@')[0]} 💀\n\nRest in pieces!`,
             mentions: [userToWaste],
-            ...channelInfo
         });
 
     } catch (error) {
         console.error('Error in wasted command:', error);
         await sock.sendMessage(chatId, { 
             text: 'Failed to create wasted image! Try again later.',
-            ...channelInfo 
+            
         });
     }
 }
