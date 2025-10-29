8427a645268f4e921dce2f84b964594c92145611
Gura <166049242+ryogura-io@users.noreply.github.com>
2025-10-29 22:53:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 const settings = {
-  packname: '𝙶𝚄𝚁𝙰-𝙼𝙳',
-  author: '💎‎',
+  packname: '𝙶𝚄𝚁𝙰',
+  author: '⸙',
   botName: "𝙶𝚄𝚁𝙰-𝙼𝙳",
-  prefix: 'x', // Prefix for commands
-  botOwner: 'Ryou', // Your name
+  prefix: '-', // Prefix for commands
+  botOwner: 'ryou', // Your name
   ownerNumber: '2347010285113', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is Ryou's personal bot.",
-  version: "ALPHA 1.0",
+  description: "This is ryou's personal bot.",
+  version: "zen",
 };
 
 module.exports = settings;
